132aa9c8050aaa459853b00d5b542996f96c6498
Ekaterina Zavyalova <ekaterina.zavyalova@tuni.fi>
2024-03-27 16:30:25 +0200

    security for the database added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mysql from 'mysql';
 import cors from 'cors';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -21,37 +24,52 @@ db.connect((err) => {
   console.log('Connected to database with id ' + db.threadId);
 });
 
+const saltRounds = 10;
 //POST
 app.post('/signup', (req, res) => {
-  const query = 'INSERT INTO login (`name`, `email`, `password`) VALUES (?)';
-  const values = [req.body.name, req.body.email, req.body.password];
-  db.query(query, [values], (err, data) => {
+  const { name, email, password } = req.body;
+  bcrypt.hash(password, saltRounds, (err, hash) => {
     if (err) {
-      console.error('Error executing query:', err);
+      console.error('Error hashing password:', err);
       return res.status(500).json({ error: err.message });
-    } else {
-      return res.json({
-        message: 'User added successfully',
-        insertId: data.insertId,
-      });
     }
+    const query = 'INSERT INTO login (`name`, `email`, `password`) VALUES (?)';
+    const values = [req.body.name, req.body.email, req.body.password];
+    db.query(query, [values], (err, data) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        return res.status(500).json({ error: err.message });
+      }   else {
+        return res.json({
+          message: 'User added successfully',
+          insertId: data.insertId,
+        });
+      }
+    });
   });
 });
 
 app.post('/login', (req, res) => {
-  const query = 'SELECT * FROM login WHERE email = ? AND password = ?';
+  const { email, password } = req.body;
+  const query = 'SELECT * FROM login WHERE email = ?';
 
-  db.query(query, [req.body.email, req.body.password], (err, data) => {
+  db.query(query, [email], (err, data) => {
     if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    if (data.length > 0) {
-      return res.json('Success');
+      // handle error...
+    } else if (data.length > 0) {
+      bcrypt.compare(password, data[0].password, (error, result) => {
+        if (result) {
+          return res.json('Success');
+        } else {
+          return res.json('Fail');
+        }
+      });
     } else {
       return res.json('Fail');
     }
   });
 });
+
 
 app.listen(8081, () => {
   console.log('Listening...');
